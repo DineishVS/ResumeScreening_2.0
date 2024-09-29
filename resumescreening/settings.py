9fca8fd95b394de35d50ca26f8f67028acc990b1
DineishVS <dineishrocky@gmail.com>
2024-09-29 21:02:47 +0530

    "added Staticfiles folder"

--- resumescreening/settings.py
+++ resumescreening/settings.py
@@ -101,11 +101,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR/'assets')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'assets')
     ]
-
+STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
